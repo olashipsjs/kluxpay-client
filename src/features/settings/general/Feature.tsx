5e5db024b8d881002f53255c15b8023278dcc6fb
olashipsjs <olanrewajuoladejo08@gmail.com>
2024-12-22 11:23:11 +0100

    fixed settings layout sidebar for mobile screens

--- src/features/settings/general/Feature.tsx
+++ src/features/settings/general/Feature.tsx
@@ -1,16 +1,13 @@
 import Heading from '@components/base/heading/Heading';
 import Currency from './components/Currency';
 import React from 'react';
+import Divider from '@components/divider/Divider';
 
 const GeneralSettingsFeature = () => {
   return (
     <React.Fragment>
-      <Heading
-        fontSize={19}
-        mb={20}
-      >
-        General Settings
-      </Heading>
+      <Heading fontSize={19}>General Settings</Heading>
+      <Divider my={16} />
       <Currency />
     </React.Fragment>
   );
